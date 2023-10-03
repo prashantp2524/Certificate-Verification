4afd534b41ba273edfa976c21acc53b8159d648c
Prashant Panchal <prashantp2524@gmail.com>
2023-10-03 15:11:33 +0530

    everything working ok

--- client/src/components/CertificateVerification.jsx
+++ client/src/components/CertificateVerification.jsx
@@ -37,6 +37,11 @@ const CertificateVerification = () =>
         console.error(error);
     };
 
+    const onclickHandler = () =>
+    {
+        setCam(true)
+        setCertificateData(null)
+    }
 
     return (
 
@@ -66,42 +71,32 @@ const CertificateVerification = () =>
                                     />
                                 ) : (
                                     <div>
+
                                         {
                                             certificateData && certificateData.map((data) => (
-                                                <h1>Hello</h1>
+                                                <div className='container'>
+                                                    <div class="shadow p-3 mb-5 bg-body rounded">
+                                                        <div class="card" style={{ width: "18rem" }} key={data.std_Id}>
+                                                            <div class="card-header">
+                                                                Student Certificate Data
+                                                            </div>
+                                                            <ul class="list-group list-group-flush">
+                                                                <li class="list-group-item">ID: {data.std_Id}</li>
+                                                                <li class="list-group-item">Name: {data.name}</li>
+                                                                <li class="list-group-item">Email: {data.email}</li>
+                                                                <li class="list-group-item">Institute: {data.institute}</li>
+                                                                <li class="list-group-item">Certificate No : {data.cert_no}</li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
                                             ))
                                         }
-                                        <p>Result: {result}</p>
-                                        <button type='button' className='btn btn-primary' onClick={() => setCam(true)}>Scan Again</button>
+                                        <button type='button' className='btn btn-primary' onClick={onclickHandler}>Scan Again</button>
                                     </div>
                                 )}
                             </div>
-                        </div>
-                        <div>
-
-                            {/* 
-                            {
-                                certificateData && certificateData.map((data) => (
-                                    // <div className='container'>
-                                    //     <div class="shadow p-3 mb-5 bg-body rounded">
-                                    //         <div class="card" style={{ width: "18rem" }} key={data.std_Id}>
-                                    //             <div class="card-header">
-                                    //                 Student Certificate Data
-                                    //             </div>
-                                    //             <ul class="list-group list-group-flush">
-                                    //                 <li class="list-group-item">ID: {data.std_Id}</li>
-                                    //                 <li class="list-group-item">Name: {data.name}</li>
-                                    //                 <li class="list-group-item">Email: {data.email}</li>
-                                    //                 <li class="list-group-item">Institute: {data.institute}</li>
-                                    //                 <li class="list-group-item">Certificate No : {data.cert_no}</li>
-                                    //             </ul>
-                                    //         </div>
-                                    //     </div>
-                                    // </div>
-                                    <h1>Hello</h1>
-
-                                ))
-                            } */}
                         </div>
                     </div>
                 </div>
