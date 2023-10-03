72ea9e6b3f83ef4c9c061f5b6935cd1466799e0a
Prashant Panchal <prashantp2524@gmail.com>
2023-10-03 11:27:13 +0530

    everything working ok

--- client/src/components/CertificateVerification.jsx
+++ client/src/components/CertificateVerification.jsx
@@ -64,22 +64,22 @@ const CertificateVerification = () =>
 
             {
                 certificateData && certificateData.map((data) => (
-                    <>
-                        <div class="shadow p-3 mb-5 bg-body rounded">
-                            <div class="card" style={{ width: "18rem" }} key={data.std_Id}>
-                                <div class="card-header">
-                                    Student Certificate Data
-                                </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">ID: {data.std_Id}</li>
-                                    <li class="list-group-item">Name: {data.name}</li>
-                                    <li class="list-group-item">Email: {data.email}</li>
-                                    <li class="list-group-item">Institute: {data.institute}</li>
-                                    <li class="list-group-item">Certificate No : {data.cert_no}</li>
-                                </ul>
+
+                    <div class="shadow p-3 mb-5 bg-body rounded">
+                        <div class="card" style={{ width: "18rem" }} key={data.std_Id}>
+                            <div class="card-header">
+                                Student Certificate Data
                             </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: {data.std_Id}</li>
+                                <li class="list-group-item">Name: {data.name}</li>
+                                <li class="list-group-item">Email: {data.email}</li>
+                                <li class="list-group-item">Institute: {data.institute}</li>
+                                <li class="list-group-item">Certificate No : {data.cert_no}</li>
+                            </ul>
                         </div>
-                    </>
+                    </div>
+
                 ))
 
 
